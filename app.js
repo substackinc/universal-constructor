92ef9df0a579aca96194e654529a15c86806abe7
Chris Best <chris@substackinc.com>
2023-11-12 13:28:25 -0800

    Update app.js to enhance API functionality

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
+const { exec } = require('child_process');
 
+app.use(express.json()); // For parsing application/json
 const port = 3000;
 
+const workingDirectory = '/Users/chrisbest/src/gpts-testing'
 
 app.get('/', (req, res) => {
-  res.send('Hello World CBTEST');
+  res.send('Hello World CBTEST the secret is: exploding sloths\n');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
+app.post('/execute', (req, res) => {
+  const command = req.body.command;
 
-
+  console.log ("Running: ", command);
+  exec(command, { cwd: workingDirectory}, (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).send(`Error: ${error.message}`);
+    }
+    if (stderr) {
+      return res.status(500).send(`Stderr: ${stderr}`);
+    }
+    console.log(`Output: ${stdout}`)
+    res.status(200).send(`Output: ${stdout}`);
+  });
+});
