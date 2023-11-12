976d7e75add3fe418233b178bd94f5af5bfc2801
Chris Best <chris@substackinc.com>
2023-11-12 12:54:42 -0800

    hello, world

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World CBTEST');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
