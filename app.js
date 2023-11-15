df80f6508d606392ca393ca31e8f0d432c80905b
Chris Best <chris@substackinc.com>
2023-11-14 20:57:23 -0800

    new /execute2 route using spawn, hopefully fixes escaping issues?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 const app = express();
 app.use(express.json()); // For parsing application/json
@@ -21,7 +21,7 @@ app.listen(port, () => {
 app.post('/execute', (req, res) => {
   const command = req.body.command;
 
-  console.log(`\n\n${new Date()}\nRunning: ${command}`)
+  console.log(`\n\n${new Date()} /execute1 \nRunning: ${command}`)
   exec(command, { cwd: workingDirectory}, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`)
@@ -33,6 +33,37 @@ app.post('/execute', (req, res) => {
     }
     console.log(`Output: ${stdout}`)
     res.status(200).send(`Output: ${stdout}`);
+  });
+});
+
+app.post('/execute2', (req, res) => {
+  const { command } = req.body;
+  const args = req.body.args || req.body.arguments || [];
+
+  console.log(`\n\n${new Date()} /execute2 \nRunning: ${command} ${args || {}}`)
+
+  // Spawn the process
+  const childProcess = spawn(command, args || [], { cwd: workingDirectory });
+
+  let output = '';
+  childProcess.stdout.on('data', (data) => {
+    console.log(data.toString())
+    output += data.toString();
+  });
+
+  childProcess.stderr.on('data', (data) => {
+    console.error(data.toString())
+    output += data.toString();
+  });
+
+  childProcess.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+    return res.status(500).send(`Error: ${error.message} \n ${output}`);
+  });
+
+  childProcess.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    res.status(200).send(output);
   });
 });
 
