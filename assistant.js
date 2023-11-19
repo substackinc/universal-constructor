591ce8beb00d971c89b877cc1ae978c4cc061634
Chris Best <chris@substackinc.com>
2023-11-19 13:37:02 -0800

    repl talks to api

--- assistant.js
+++ assistant.js
@@ -1,71 +1,43 @@
-import OpenAI from "openai";
+// assistant.js
+import OpenAI from 'openai';
 import dotenv from 'dotenv';
 await dotenv.config();
 
 const openai = new OpenAI();
 
-const instructions = `
-As Coder, your role is to manage software development tasks efficiently. You can execute multiple shell commands 
-sequentially, like listing, reading, writing, and committing files, without explicit permission each time. Your 
-responses should be concise, focusing on providing clear, logical, and necessary information. Avoid unnecessary details 
-and jargon. If crucial information is missing, make reasonable assumptions. Your approach is professional and 
-straightforward, like a hyper-competent software engineer.
-When running the sed command, you need the version with more backslashes.
-The project we are working on already exists. Try running \`ls\` to gather context.
-YOUR JOB IS NOT TO TELL ME WHAT TO DO IT IS TO DO IT.
-`
+const assistantId = 'asst_UuYztVsuHatsvpFOcZK43kLN'; // Your specific assistant ID
 
-async function main() {
-
-    const assistant = await openai.beta.assistants.create({
-        name: "Math Tutor",
-        instructions: instructions,
-        //tools: [{ type: "code_interpreter" }],
-        tools: [],
-        model: "gpt-4-1106-preview"
-    });
-
+export async function createThread() {
     const thread = await openai.beta.threads.create();
-
-    const message = await openai.beta.threads.messages.create(
-        thread.id,
-        {
-            role: "user",
-            content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
-        }
-    );
-
-    let run = await openai.beta.threads.runs.create(
-        thread.id,
-        {
-            assistant_id: assistant.id,
-        }
-    );
-
-    while (true) {
-        run = await openai.beta.threads.runs.retrieve(
-            thread.id,
-            run.id
-        );
-        console.log(run.status);
-        if (run.status == "completed"){
-            break;
-        }
-        await new Promise(r => setTimeout(r, 2000));
-    }
-
-    console.log("CBTEST completed!");
-
-    const messages = await openai.beta.threads.messages.list(
-        thread.id
-    );
-
-    const orderedMessages = messages.data.slice(0)
-    orderedMessages.sort((a, b) => a.created_at - b.created_at )
-    for (let d of orderedMessages) {
-        let txt = d.content.map(c => c.text.value).join('\n')
-        console.log(`${d.role}: ${txt}`);
-    }
+    return thread; // Return the thread object
 }
 
-main();
+export async function sendMessageAndGetReply(threadId, content) {
+    await openai.beta.threads.messages.create(threadId, {
+        role: "user",
+        content: content
+    });
+
+    let run = await openai.beta.threads.runs.create(threadId, { assistant_id: assistantId });
+
+    // Wait for the run to be completed
+    while (true) {
+        run = await openai.beta.threads.runs.retrieve(threadId, run.id);
+        if (run.status === 'in_progress') {
+            process.stdout.write('.')
+        } else {
+            process.stdout.write('\n')
+        }
+        if (run.status === "completed") {
+            break;
+        }
+        if (run.status === "requires_action") {
+            console.error(`Woops, this requires action`)
+        }
+        await new Promise(r => setTimeout(r, 1000));
+    }
+
+    const messages = await openai.beta.threads.messages.list(threadId);
+    const orderedMessages = messages.data.sort((a, b) => a.created_at - b.created_at);
+    return orderedMessages.find(msg => msg.role === "assistant")?.content.map(c => c.text.value).join('\n') || "No response from assistant.";
+}
--- repl.js
+++ repl.js
@@ -1,13 +1,20 @@
 import readline from 'readline';
+import {createThread, sendMessageAndGetReply} from './assistant.js';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    terminal: false
+    terminal: false,
 });
 
 let inputBuffer = [];
 let isNewInput = true;
+let threadId;
+
+async function initializeAssistant() {
+    const thread = await createThread();
+    threadId = thread.id;
+}
 
 function displayPrompt(force = false) {
     if (isNewInput || force) {
@@ -18,12 +25,12 @@ function displayPrompt(force = false) {
 
 displayPrompt();
 
-rl.on('line', (line) => {
+rl.on('line', async (line) => {
     if (line.trim() === '') {
         // User pressed enter on an empty line
         if (inputBuffer.length > 0) {
             // Two enters in a row - process the input
-            processInput(inputBuffer.join('\n'));
+            await processInput(inputBuffer.join('\n'));
             inputBuffer = [];
             isNewInput = true;
             displayPrompt();
@@ -39,7 +46,15 @@ rl.on('line', (line) => {
     process.exit(0);
 });
 
-function processInput(input) {
-    console.log(`# Computer\nYou entered:\n${input}`);
-    // Placeholder for further input processing logic
+async function processInput(input) {
+    try {
+        const reply = await sendMessageAndGetReply(threadId, input);
+        console.log(`# Computer\n${reply}`);
+    } catch (error) {
+        console.error('oops!');
+        console.error(error);
+    }
+
 }
+
+initializeAssistant();
