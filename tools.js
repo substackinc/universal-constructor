cd19fc28627f7d72da73296ac295738167db833f
Chris Best <chris@substackinc.com>
2023-11-22 23:17:47 -0800

    update_file seems to help!

--- tools.js
+++ tools.js
@@ -120,7 +120,118 @@ async function write_file({ filepath, content, placeholder = '' }) {
     }
 }
 
-const tools = [{ "type": "retrieval" }, exec_shell_spec, write_file_spec];
-const toolsDict = { exec_shell, write_file };
+const update_file_spec = {
+    "type": "function",
+    "function": {
+        "name": "update_file",
+        "description": "Updates lines in a specified file with multiple changes, targeting lines by their exact content.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "filepath": {
+                    "type": "string",
+                    "description": "The path to the file within the working directory"
+                },
+                "changes": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "content": {
+                                "type": "string",
+                                "description": "The content to write or insert"
+                            },
+                            "targetLines": {
+                                "type": "string",
+                                "description": "Exact content of the target lines for modification"
+                            },
+                            "action": {
+                                "type": "string",
+                                "enum": ["before", "after", "replace"],
+                                "description": "Operation to perform relative to the target lines: insert before, insert after, or replace"
+                            }
+                        },
+                        "required": ["content", "targetLines", "action"]
+                    }
+                }
+            },
+            "required": ["filepath", "changes"]
+        }
+    }
+};
+
+function findSubsequences(array, subsequence) {
+    let subsequences = [];
+    for (let i = 0; i <= array.length - subsequence.length; i++) {
+        let foundMatch = true;
+        for (let j = 0; j < subsequence.length; j++) {
+            if (array[i + j] !== subsequence[j]) {
+                foundMatch = false;
+                break;
+            }
+        }
+        if (foundMatch) {
+            subsequences.push(i) // Return the starting index of the run
+        }
+    }
+    return subsequences;
+}
+
+async function update_file({ filepath, changes }) {
+    console.log("Updating", filepath);
+    const fullPath = resolve(workingDirectory, filepath);
+    let fileContents = await fs.readFile(fullPath, 'utf8');
+    let fileLines = fileContents.split('\n');
+
+    for (const change of changes) {
+        const { content, targetLines, action } = change;
+        const lines = targetLines.split('\n')
+        const foundAt = findSubsequences(fileLines, lines)
+
+        if (foundAt.length === 0) {
+            console.error(`Could not find target: ${targetLines}`)
+            return {
+                success: false,
+                message: `Could not find target: ${targetLines}`
+            }
+        }
+        else if (foundAt.length > 1) {
+            console.error(`Found multiple ${foundAt.length} instances of: ${targetLines}`);
+            return {
+                success: false,
+                message: `Found multiple ${foundAt.length} instances of: ${targetLines}`
+            }
+        }
+
+        const [targetIndex] = foundAt;
+        const replacementLines = content.split('\n');
+
+        switch (action) {
+            case 'before':
+                fileLines.splice(targetIndex, 0, ...replacementLines);
+                break;
+            case 'after':
+                fileLines.splice(targetIndex + 1, 0, ...replacementLines);
+                break;
+            case 'replace':
+                fileLines.splice(targetIndex, replacementLines.length, ...replacementLines);
+                break;
+            default:
+                console.error(`Unknown action specified: ${action}`);
+                throw new Error(`Action not supported: ${action}`);
+        }
+    }
+
+    await fs.writeFile(fullPath, fileLines.join('\n'), 'utf8');
+
+    return {
+        success: true,
+        message: "Successfully applied changes to the file.",
+        newContents: fileLines.join('\n')
+    };
+}
+
+const tools = [{ "type": "retrieval" }, exec_shell_spec, write_file_spec, update_file_spec];
+const toolsDict = { exec_shell, write_file, update_file };
 
 export { tools, toolsDict };
