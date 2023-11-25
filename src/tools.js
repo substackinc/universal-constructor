923598a9490b6fb82d8a122a36b8b8fccd048297
Chris Best <chris@substackinc.com>
2023-11-24 23:54:05 -0800

    let UC design it's own API, works much better

--- src/tools.js
+++ src/tools.js
@@ -98,126 +98,6 @@ async function write_file(args) {
     }
 }
 
-const update_file_spec = {
-    type: 'function',
-    function: {
-        name: 'update_file',
-        description:
-            'Updates a specified target portion of a file. Can handle direct character range updates or target-based updates with an optional index for multiple matches.',
-        parameters: {
-            type: 'object',
-            properties: {
-                filepath: {
-                    type: 'string',
-                    description: 'The path to the file within the working directory',
-                },
-                changes: {
-                    type: 'array',
-                    items: {
-                        type: 'object',
-                        properties: {
-                            content: {
-                                type: 'string',
-                                description: 'The content to write or insert',
-                            },
-                            range: {
-                                type: 'string',
-                                description:
-                                    'The character range to replace, expressed as "start:end". Exclusive of the end index.',
-                                pattern: '^[0-9]+:[0-9]+$',
-                            },
-                            target: {
-                                type: 'string',
-                                description:
-                                    'Exact content of the target section for modification when not using range. Can be multiple lines.',
-                            },
-                            action: {
-                                type: 'string',
-                                enum: ['before', 'after', 'replace'],
-                                description:
-                                    'Operation to perform relative to the target or range: insert before, insert after, or replace',
-                            },
-                            targetInstanceNumber: {
-                                type: 'integer',
-                                description:
-                                    'The 0-based index of the target instance to replace when there are multiple matches. Omit if only one match exists or when using range.',
-                            },
-                        },
-                        required: ['content', 'action'],
-                        additionalProperties: false,
-                    },
-                },
-            },
-            required: ['filepath', 'changes'],
-        },
-    },
-};
-
-async function update_file({ filepath, changes }) {
-    log(`Updating ${filepath}`)
-    const fullPath = resolve(workingDirectory, filepath);
-    let fileContents = await fs.readFile(fullPath, 'utf8');
-
-    for (const change of changes) {
-        log(change);
-        const { content, range, target, action, targetInstanceNumber } = change;
-
-        if (range) {
-            // If a range is specified, use it directly for string manipulation
-            const [start, end] = range.split(':').map(Number);
-            switch (action) {
-                case 'before':
-                    fileContents = fileContents.substring(0, start) + content + fileContents.substring(start);
-                    break;
-                case 'after':
-                    fileContents = fileContents.substring(0, end) + content + fileContents.substring(end);
-                    break;
-                case 'replace':
-                    fileContents = fileContents.substring(0, start) + content + fileContents.substring(end);
-                    break;
-                default:
-                    throw new Error(`Unsupported action: ${action}`);
-            }
-        } else if (target !== undefined) {
-            // If a target is specified, find it in the file to determine the range
-            let startIndex = fileContents.indexOf(target);
-            if (startIndex === -1) {
-                throw new Error(`Target not found: ${target}`);
-            }
-            if (targetInstanceNumber) {
-                // Skip the specified number of occurrences to find the nth instance
-                for (let i = 0; i < targetInstanceNumber; i++) {
-                    startIndex = fileContents.indexOf(target, startIndex + 1);
-                    if (startIndex === -1) {
-                        throw new Error(
-                            `Target instance number ${targetInstanceNumber} not found for target: ${target}`
-                        );
-                    }
-                }
-            }
-            const endIndex = startIndex + target.length;
-            switch (action) {
-                case 'before':
-                    fileContents = fileContents.substring(0, startIndex) + content + fileContents.substring(startIndex);
-                    break;
-                case 'after':
-                    fileContents = fileContents.substring(0, endIndex) + content + fileContents.substring(endIndex);
-                    break;
-                case 'replace':
-                    fileContents = fileContents.substring(0, startIndex) + content + fileContents.substring(endIndex);
-                    break;
-                default:
-                    throw new Error(`Unsupported action: ${action}`);
-            }
-        } else {
-            throw new Error('Update change requires either a range or a target.');
-        }
-    }
-
-    await fs.writeFile(fullPath, fileContents);
-    return { success: true, newContents: fileContents };
-}
-
 // Function to retrieve the full content of a file and some relevant info.
 const show_file_spec = {
     type: 'function',
@@ -317,9 +197,7 @@ async function search_file({ filepath, search }) {
     while ((match = searchRegex.exec(fileContent)) !== null) {
         const contextStartIndex = Math.max(match.index - 1, 0);
         const contextEndIndex = searchRegex.lastIndex;
-        const range = `${match.index}-${searchRegex.lastIndex - 1}`;
         matches.push({
-            range,
             context: fileContent.substring(contextStartIndex, contextEndIndex),
         });
     }
@@ -327,18 +205,61 @@ async function search_file({ filepath, search }) {
     return matches;
 }
 
+const replaceInFile_spec = {
+    name: 'replaceInFile',
+    description:
+        'Replaces a specific substring within a given search context in a file, ensuring that the context appears only once.',
+    parameters: {
+        filepath: 'The relative path to the file where the replacement should occur.',
+        searchContext:
+            'The surrounding context where the target substring is located. This should appear exactly once in the file.',
+        targetSubstring: 'The exact substring within the search context that needs to be replaced.',
+        replacement: 'The text that should replace the target substring.',
+    },
+    required: ['filepath', 'searchContext', 'targetSubstring', 'replacement'],
+};
+
+async function replaceInFile({ filepath, searchContext, targetSubstring, replacement }) {
+    log(`Replacing in "${filepath}`);
+    log({searchContext, targetSubstring, replacement})
+    const fullPath = resolve(workingDirectory, filepath);
+    const fileContents = await fs.readFile(fullPath, 'utf8');
+
+    // Ensure the searchContext appears only once in the fileContents.
+    const occurrences = fileContents.match(new RegExp(escapeStringRegexp(searchContext), 'g')) || [];
+    if (occurrences.length !== 1) {
+        throw new Error('The search context must appear exactly once in the file, but found ' + occurrences.length);
+    }
+
+    // Replace targetSubstring with replacement within the searchContext.
+    const updatedContext = searchContext.replace(targetSubstring, replacement);
+
+    // Replace the old searchContext with the updatedContext in the fileContents.
+    const updatedFileContents = fileContents.replace(searchContext, updatedContext);
+
+    // Write the updated contents back to the file.
+    await fs.writeFile(fullPath, updatedFileContents, 'utf8');
+
+    return {
+        success: true,
+        filepath,
+        searchContext,
+        updatedContext
+    }
+}
+
 // Exported tools and their corresponding functions
 const tools = [
     { type: 'retrieval' },
     exec_shell_spec,
     write_file_spec,
-    update_file_spec,
     show_file_spec,
     get_summary_spec,
     search_file_spec,
+    replaceInFile_spec,
     { name: 'log', function: log },
 ];
 
-const toolsDict = { exec_shell, write_file, update_file, show_file, get_summary, search_file };
+const toolsDict = { exec_shell, write_file, replaceInFile, show_file, get_summary, search_file };
 
 export { tools, toolsDict };
