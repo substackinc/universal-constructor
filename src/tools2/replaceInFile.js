c320bc9aa065e60f49a1f2bd1d967fc4cfec2285
Chris Best <chris@substackinc.com>
2023-11-25 17:18:45 -0800

    fixed replaceInFile (manual)

--- src/tools2/replaceInFile.js
+++ src/tools2/replaceInFile.js
@@ -1,42 +1,58 @@
 // src/tools2/replaceInFile.js
 import fs from 'fs/promises';
 import path from 'path';
-import escapeStringRegexp from 'escape-string-regexp';
 
 replaceInFile.spec = {
-  name: 'replaceInFile',
-  description: 'Replaces a specific substring within a given search context in a file, ensuring that the context appears only once.',
-  parameters: {
-    filepath: {
-      type: 'string',
-      description: 'The relative path to the file where the replacement should occur.',
+    name: 'replaceInFile',
+    description:
+        'Replaces a specific substring within a given search context in a file, ensuring that the context appears only once.',
+    parameters: {
+        filepath: {
+            type: 'string',
+            description: 'The relative path to the file where the replacement should occur.',
+        },
+        searchContext: {
+            type: 'string',
+            description:
+                'The surrounding context where the target substring is located. This should appear exactly once in the file.',
+        },
+        targetSubstring: {
+            type: 'string',
+            description: 'The exact substring within the search context that needs to be replaced.',
+        },
+        replacement: {
+            type: 'string',
+            description: 'The text that should replace the target substring.',
+        },
     },
-    searchContext: {
-      type: 'string',
-      description: 'The surrounding context where the target substring is located. This should appear exactly once in the file.',
-    },
-    targetSubstring: {
-      type: 'string',
-      description: 'The exact substring within the search context that needs to be replaced.',
-    },
-    replacement: {
-      type: 'string',
-      description: 'The text that should replace the target substring.',
-    },
-  },
 };
 
 export default async function replaceInFile({ filepath, searchContext, targetSubstring, replacement }) {
-  const fullPath = path.resolve(filepath);
-  let fileContents = await fs.readFile(fullPath, 'utf8');
-  const regex = new RegExp(escapeStringRegexp(searchContext), 'g');
-  if (!regex.test(fileContents)) {
-    throw new Error(`The search context:\n${searchContext}\nwas not found in the file.`);
-  }
-  fileContents = fileContents.replace(new RegExp(escapeStringRegexp(targetSubstring), 'g'), replacement);
-  await fs.writeFile(fullPath, fileContents, 'utf8');
-  return {
-    success: true,
-    updatedContent: fileContents,
-  };
+    const fullPath = path.resolve(filepath);
+    const fileContents = await fs.readFile(fullPath, 'utf8');
+
+    const searchContextIndex = fileContents.indexOf(searchContext);
+    if (searchContextIndex === -1) {
+        console.log('CBCTEST context', searchContext);
+        console.log('CBCTEST fileContent', fileContents);
+        throw new Error('searchContext not found in file');
+    } else if (searchContextIndex != fileContents.lastIndexOf(searchContext)) {
+        throw new Error('searchContext found more than once in the file');
+    }
+
+    const targetSubstringIndex = searchContext.indexOf(targetSubstring);
+    if (targetSubstringIndex === -1) {
+        throw new Error('targetSubstring does not appear in the searchContext');
+    } else if (targetSubstringIndex != searchContext.lastIndexOf(targetSubstring)) {
+        throw new Error('targetSubstring appears more than once in the searchContext');
+    }
+
+    let editedContext = searchContext.replace(targetSubstring, replacement);
+    let updatedFileContents = fileContents.replace(searchContext, editedContext);
+
+    await fs.writeFile(fullPath, updatedFileContents, 'utf8');
+    return {
+        success: true,
+        updatedContent: updatedFileContents,
+    };
 }
--- test/tools2/replaceInFile.test.js
+++ test/tools2/replaceInFile.test.js
@@ -2,28 +2,54 @@ import test from 'ava';
 import replaceInFile from '../../src/tools2/replaceInFile.js';
 import fs from 'fs/promises';
 import path from 'path';
+import os from 'os';
 
-const testDir = path.resolve('test/tmp');
-const testFile = path.join(testDir, 'testReplaceInFile.tmp.txt');
+const testDir = path.join(os.tmpdir(), 'replaceInFileTests');
+let i = 1;
+
+function setup() {
+    return {
+        testFile: path.join(testDir, `testReplaceInFile.tmp${i++}.txt`),
+    };
+}
 
 // Creating a temporary test file before the tests
 test.before(async () => {
-  await fs.writeFile(testFile, 'Hello World', 'utf8');
+    await fs.mkdir(testDir, { recursive: true });
 });
 
 // Cleanup: remove the temporary file and directory after the tests
 test.after.always(async () => {
-  await fs.unlink(testFile);
-  await fs.rmdir(testDir);
+    await fs.rm(testDir, { force: true, recursive: true });
 });
 
-test('replaceInFile replaces a string within a file', async t => {
-  await replaceInFile({
-    filepath: testFile,
-    searchContext: 'Hello World',
-    targetSubstring: 'World',
-    replacement: 'AVA'
-  });
-  const content = await fs.readFile(testFile, 'utf8');
-  t.is(content, 'Hello AVA', 'Content should be replaced correctly');
+test('replaceInFile replaces a string within a file', async (t) => {
+    const { testFile } = setup();
+    await fs.writeFile(testFile, 'Hello World, Hello Universe', 'utf8');
+    let contents = await fs.readFile(testFile, 'utf8');
+    console.log('CBTEST', testFile, contents);
+    await replaceInFile({
+        filepath: testFile,
+        searchContext: 'Hello World, Hello Universe',
+        targetSubstring: 'Universe',
+        replacement: 'AVA',
+    });
+    const content = await fs.readFile(testFile, 'utf8');
+    t.is(content, 'Hello World, Hello AVA', 'Content should be replaced correctly');
+});
+
+test('replaceInFile fails with multiple instances of the search context', async (t) => {
+    const { testFile } = setup();
+    await fs.writeFile(testFile, 'Hello World. Hello World.', 'utf8');
+    try {
+        await replaceInFile({
+            filepath: testFile,
+            searchContext: 'Hello World',
+            targetSubstring: 'World',
+            replacement: 'AVA',
+        });
+        t.fail('replaceInFile should throw an error if the search context appears more than once.');
+    } catch (error) {
+        t.pass('replaceInFile should throw an error if the search context appears more than once.');
+    }
 });
