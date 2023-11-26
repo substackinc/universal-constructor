504065802005abbf0c52a2ed2f49d411f7fc5aa7
Chris Best <chris@substackinc.com>
2023-11-25 17:47:59 -0800

    searchFile (UC wrote, but took several tries and heavy prompting)

--- src/tools2/searchFile.js
+++ src/tools2/searchFile.js
@@ -1,47 +1,44 @@
 // src/tools2/searchFile.js
+import fs from 'fs/promises';
+import path from 'path';
 
 searchFile.spec = {
-    name: 'search_file',
-    description: 'Searches for a string in a file and returns all matches with context.',
-    parameters: {
-        filepath: {
-            type: 'string',
-            description: 'The path to the file within the working directory.',
-        },
-        search: {
-            type: 'string',
-            description: 'The search string to find in the file.',
-        },
-        contextBefore: {
-            type: 'integer',
-            description: 'Lines of context to return before the match. (Default 5)',
-        },
-        contextAfter: {
-            type: 'integer',
-            description: 'Lines of context to return after the match. (Default 5)',
-        },
+  name: 'search_file',
+  description: 'Searches for a string in a file and returns all matches.',
+  parameters: {
+    filepath: {
+      type: 'string',
+      description: 'The path to the file within the working directory.',
     },
+    search: {
+      type: 'string',
+      description: 'The search string to find in the file.',
+    }
+  }
 };
 
-export default async function searchFile({ filepath, search, contextBefore = 5, contextAfter = 5 }) {
-    const fs = require('fs').promises;
-    const escapeStringRegexp = require('escape-string-regexp');
-    // Tool implementation
-    const fileContent = await fs.readFile(filepath, 'utf8');
-    const searchRegex = new RegExp(
-        `(?:.*\\n){0,${contextBefore}}(.*${escapeStringRegexp(search)}.*)(?:\\n.*){0,${contextAfter}}`,
-        'gi'
-    );
-    let matches = [];
-    let match;
-    while ((match = searchRegex.exec(fileContent)) !== null) {
-        let [context, line] = match;
-        matches.push({ line, context });
+export default async function searchFile({ filepath, search }) {
+  const fullPath = path.resolve(filepath);
+  const fileContent = await fs.readFile(fullPath, 'utf8');
+  const splitContent = fileContent.split(search);
+  let matches = [];
+
+  splitContent.forEach((content, index) => {
+    if (index < splitContent.length - 1) { // Ignore the last split part as it won't be followed by the search term
+      const startContext = content.length > 30 ? content.slice(-30) : content;
+      const endContextIndex = splitContent[index + 1].length > 30 ? 30 : splitContent[index + 1].length;
+      const endContext = splitContent[index + 1].slice(0, endContextIndex);
+      matches.push({
+        found: `${startContext}\u001b[1m${search}\u001b[22m${endContext}`, // Include search term in bold
+        context: `${content.slice(-100)}${search}${splitContent[index + 1].slice(0, 100)}` // Provide 100 chars of context
+      });
     }
-    return {
-        success: true,
-        filepath,
-        search,
-        matches,
-    };
+  });
+
+  return {
+    success: true,
+    filepath,
+    search,
+    matches,
+  };
 }
---
+++ test/tools2/searchFile.test.js
@@ -0,0 +1,41 @@
+import test from 'ava';
+import searchFile from '../../src/tools2/searchFile.js';
+import fs from 'fs/promises';
+import path from 'path';
+import os from 'os';
+
+const testDir = path.join(os.tmpdir(), 'searchFileTests');
+const testFilePath = path.join(testDir, 'testSearchFile.tmp.txt');
+const testContent = 'First line\nSecond line with Search term\nThird line with Search term\nFourth line\nFifth line with Search term';
+
+// Create a temporary test directory and file before the tests
+test.before(async () => {
+  await fs.mkdir(testDir, { recursive: true });
+  await fs.writeFile(testFilePath, testContent, 'utf8');
+});
+
+// Cleanup test directory and file after the tests
+test.after.always(async () => {
+  await fs.rm(testDir, { recursive: true, force: true });
+});
+
+test('searchFile finds and returns all matches with search term', async t => {
+  const { matches } = await searchFile({
+    filepath: testFilePath,
+    search: 'Search term',
+  });
+  t.is(matches.length, 3, 'Should find three lines with the search term');
+
+  for (const match of matches) {
+    t.true(match.found.includes('Search term'), 'found should include the search term');
+    t.true(match.context.length > 0, 'There should be some context for each match');
+  }
+});
+
+test('searchFile returns no matches when search term is not present', async t => {
+  const { matches } = await searchFile({
+    filepath: testFilePath,
+    search: 'nonexistent',
+  });
+  t.is(matches.length, 0, 'Should not find matches with a nonexistent search term');
+});
