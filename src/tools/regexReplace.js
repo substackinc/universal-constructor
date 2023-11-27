9a6b0ac47eda10c914d6796ec9b8b9d7161f66f1
Chris Best <chris@substackinc.com>
2023-11-26 20:35:01 -0800

    wip regex

--- src/tools/regexReplace.js
+++ src/tools/regexReplace.js
@@ -1,8 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 regexReplace.spec = {
     name: regexReplace.name,
