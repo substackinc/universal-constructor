df83cd6e18ab6ceea5f142c901311c50dd78ded1
Chris Best <chris@substackinc.com>
2023-11-26 22:46:01 -0800

    format

--- test/tools/regexReplace.test.js
+++ test/tools/regexReplace.test.js
@@ -4,59 +4,60 @@ import { promises as fsPromises } from 'fs';
 import path from 'path';
 import fs from 'fs/promises';
 import os from 'os';
+
 const { writeFile, readFile } = fsPromises;
 
-let i=0
+let i = 0;
 const testDir = path.join(os.tmpdir(), 'replaceInFileTests');
 const content = 'Hello World! Hello People! Hello Universe!';
 
 async function setup() {
-  let testFile = path.join(testDir, `test_regex_replace.tmp${i++}.txt`);
-  await writeFile(testFile, content);
-  return {testFile}
+    let testFile = path.join(testDir, `test_regex_replace.tmp${i++}.txt`);
+    await writeFile(testFile, content);
+    return { testFile };
 }
 
 // Creating a temporary test file before the tests
 test.before(async () => {
-  await fs.mkdir(testDir, { recursive: true });
+    await fs.mkdir(testDir, { recursive: true });
 });
 
 // Cleanup: remove the temporary file and directory after the tests
 test.after.always(async () => {
-  await fs.rm(testDir, { force: true, recursive: true });
+    await fs.rm(testDir, { force: true, recursive: true });
 });
 
 test('regexReplace - replaces content matching a regex pattern without dryRun', async (t) => {
-  const {testFile} = await setup();
-  const result = await regexReplace({
-    regex: '/Hello/g',
-    filepath: testFile,
-    replacement: 'Hi',
-    dryRun: false
-  });
+    const { testFile } = await setup();
+    const result = await regexReplace({
+        regex: '/Hello/g',
+        filepath: testFile,
+        replacement: 'Hi',
+        dryRun: false,
+    });
 
-  const updatedContent = await readFile(testFile, 'utf-8');
+    const updatedContent = await readFile(testFile, 'utf-8');
 
-  t.true(result.success);
-  t.is(result.matchCount, 3);
-  t.is(updatedContent, 'Hi World! Hi People! Hi Universe!');
+    t.true(result.success);
+    t.is(result.matchCount, 3);
+    t.is(updatedContent, 'Hi World! Hi People! Hi Universe!');
 });
 
 test('regexReplace - dryRun should not change the file content', async (t) => {
-  const {testFile} = await setup();
-  const result = await regexReplace({
-    regex: '/Hello/g',
-    filepath: testFile,
-    replacement: 'Hi',
-    dryRun: true
-  });
+    const { testFile } = await setup();
+    const result = await regexReplace({
+        regex: '/Hello/g',
+        filepath: testFile,
+        replacement: 'Hi',
+        dryRun: true,
+    });
 
-  const unchangedContent = await readFile(testFile, 'utf-8');
+    const unchangedContent = await readFile(testFile, 'utf-8');
 
-  t.true(result.success);
-  t.is(result.matchCount, 3);
-  t.is(result.originalContent, content);
-  t.is(unchangedContent, content);
-  t.is(result.updatedContent, 'Hi World! Hi People! Hi Universe!', 'dry run lets me see what it would have done');
-  t.falsy(result.fileUpdated)
+    t.true(result.success);
+    t.is(result.matchCount, 3);
+    t.is(result.originalContent, content);
+    t.is(unchangedContent, content);
+    t.is(result.updatedContent, 'Hi World! Hi People! Hi Universe!', 'dry run lets me see what it would have done');
+    t.falsy(result.fileUpdated);
 });
