375dcdb96f635b329f76cdddb60c7769e5086bd6
Chris Best <chris@substackinc.com>
2023-11-29 12:32:44 -0800

    fix test

--- test/tools/getSummary.test.js
+++ test/tools/getSummary.test.js
@@ -14,7 +14,7 @@ test('getSummary provides a summary object with the necessary keys', async (t) =
     t.truthy(summary.shell_results['cat priorities.md'].stdout, 'Should have stdout for icat priorities.md');
     t.is(summary.shell_results['cat priorities.md'].exitCode, 0, 'Exit code 0');
     t.truthy(summary.shell_results['git ls-files'], 'Should have git ls-files results');
-    t.truthy(summary.shell_results['git status'], 'Should have git status results');
+    t.truthy(summary.shell_results['git status -u'], 'Should have git status results');
     t.truthy(summary.shell_results['git log -n 5'], 'Should have git log -n 5 results');
     t.truthy(summary.shell_results['cat package.json'], 'Should have cat package.json results');
     // t.truthy(summary.shell_results['prettier-check'], 'Should have prettier --check . results');
