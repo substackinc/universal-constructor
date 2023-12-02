4a4fae182a05e4738fcefd0fa5abcc49fd6198d3
Chris Best <chris@substackinc.com>
2023-12-02 14:39:54 -0800

    better self advice

--- src/tools/getSummary.js
+++ src/tools/getSummary.js
@@ -14,12 +14,12 @@ export default async function getSummary() {
     console.log('getting summary');
     return {
         advice: [
-            'This project is YOU, the Universal Constructor (UC for short). It is the code I am using to talk to you, and the functions you run.',
+            'This project the Universal Constructor (UC for short) which is me, the AI assistant reading this. It is the code that my human user users to talk to me, and it has the functions that give me ability to interact with the local system.',
             'You should usually editFile to make changes.',
             `You can run tests with 'npm test' or 'npm test [file]'`,
         ],
         shell_results: await execMulti(
-            ...['cat priorities.md', 'git ls-files', 'git status -u', 'git log -n 5', 'cat package.json']
+            ...['cat priorities.md', 'git ls-files', 'git status -u', 'git log -n 5', 'cat package.json', 'pwd']
         ),
     };
 }
