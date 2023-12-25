9b7465e422d163454a595ec2275ffc57e7140702
Chris Best <chris@substackinc.com>
2023-12-24 21:03:23 -0800

    fix test

--- test/tools/getSummary.test.js
+++ test/tools/getSummary.test.js
@@ -10,9 +10,10 @@ test('getSummary provides a summary object with the necessary keys', async (t) =
     t.truthy(summary.shell_results, 'Summary should have a shell_results key');
 
     // Checking that all shell commands have run
-    t.truthy(summary.shell_results['cat priorities.md'], 'Should have cat priorities.md results');
-    t.truthy(summary.shell_results['cat priorities.md'].stdout, 'Should have stdout for icat priorities.md');
-    t.is(summary.shell_results['cat priorities.md'].exitCode, 0, 'Exit code 0');
+    // The following checks are commented out because the cat priorities.md command may not exist in all working directories
+    t.truthy(summary.shell_results['cat *.md'], 'Should have cat *.md results');
+    t.truthy(summary.shell_results['cat *.md'].stdout, 'Should have stdout for cat *.md');
+    t.is(summary.shell_results['cat *.md'].exitCode, 0, 'Exit code 0');
     t.truthy(summary.shell_results['git ls-files'], 'Should have git ls-files results');
     t.truthy(summary.shell_results['git status -u'], 'Should have git status results');
     t.truthy(summary.shell_results['git log -n 5'], 'Should have git log -n 5 results');
