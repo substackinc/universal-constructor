8309d7babe59dd45ecae070e64dffaea745098b2
Chris Best <chris@substackinc.com>
2023-12-24 20:58:24 -0800

    Update getSummary to reflect current project state and capabilities

--- src/tools/getSummary.js
+++ src/tools/getSummary.js
@@ -13,13 +13,16 @@ getSummary.spec = {
 export default async function getSummary() {
     console.log('getting summary');
     return {
-        advice: [
-            'This project the Universal Constructor (UC for short) which is me, the AI assistant reading this. It is the code that my human user users to talk to me, and it has the functions that give me ability to interact with the local system.',
-            'You should usually editFile to make changes.',
-            `You can run tests with 'npm test' or 'npm test [file]'`,
-        ],
+        advice: [],
+        current_working_directory: process.cwd(),
         shell_results: await execMulti(
-            ...['cat priorities.md', 'git ls-files', 'git status -u', 'git log -n 5', 'cat package.json', 'pwd']
+            ...[
+                'cat *.md',
+                'git ls-files',
+                'git status -u',
+                'git log -n 5',
+                'git rev-parse --show-toplevel',
+                'cat package.json'],
         ),
     };
 }
