236867fa777cb28de398880fe388007d6c3ac50c
Chris Best <chris@substackinc.com>
2023-12-24 18:00:19 -0800

    let UC change it's working directory

---
+++ src/tools/setWorkingDirectory.js
@@ -0,0 +1,51 @@
+// src/tools/setWorkingDirectory.js
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * Sets the working directory for the process.
+ *
+ * @hoisted true
+ */
+setWorkingDirectory.spec = {
+  name: 'setWorkingDirectory',
+  description: 'Sets the working directory for all subsequent tool operations.',
+  parameters: {
+    type: 'object',
+    properties: {
+      dir: {
+        type: 'string',
+        description: 'The path to the directory to use as the working directory.',
+      },
+    },
+    required: ['dir'],
+  },
+};
+
+/**
+ * Sets the working directory for all subsequent tool operations.
+ *
+ * @param {object} options - The options for setting the working directory.
+ * @param {string} options.dir - The path to the directory to use as the working directory.
+ * @returns {void}
+ */
+export default async function setWorkingDirectory({ dir }) {
+  if (!dir || typeof dir !== 'string') {
+    throw new Error('You must provide a directory path as a string.');
+  }
+
+  if (!path.isAbsolute(dir)) {
+    dir = path.join(process.cwd(), dir);
+  }
+
+  if (!fs.existsSync(dir)) {
+    throw new Error('The specified directory does not exist.');
+  }
+
+  process.chdir(dir);
+  console.log("Changed directory to: ", process.cwd())
+  return {
+    success: true,
+    cwd: process.cwd()
+  }
+}
