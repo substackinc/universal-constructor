7111d48682d7d29f98dec8889f26bf0b076041a2
Chris Best <chris@substackinc.com>
2024-02-03 16:27:41 -0800

    better truncation

--- src/speaker.js
+++ src/speaker.js
@@ -20,17 +20,7 @@ async function speak(text, truncate=200) {
     if (truncate > 0 && text.length > truncate) {
         // last para
         text = text.split('\n').pop();
-
-        while (text.length > truncate) {
-            let sentences = text.split('. ');
-            if (sentences.length > 1) {
-                text = sentences.slice(1).join('. ');
-            }
-            else {
-                // be brutal
-                text = text.slice(-truncate)
-            }
-        }
+        text = truncateSentences(text, truncate);
     }
     //console.log('CBTEST truncate', truncate, text);
 
@@ -49,6 +39,25 @@ async function speak(text, truncate=200) {
     await play(speechFile);
     fs.unlinkSync(speechFile);
 }
+
+function truncateSentences(text, characterLimit) {
+    const sentences = text.match(/[^.!?]+[.!?]\s*/g);
+    if (!sentences) return '';
+
+    let truncated = text;
+    while (truncated.length > characterLimit && sentences.length > 1) {
+        sentences.shift();
+        truncated = sentences.join('');
+    }
+
+    if (truncated.length > characterLimit && sentences.length === 1) {
+        // ehh screw it.
+        truncated.slice(-characterLimit)
+    }
+
+    return truncated.trim();
+}
+
 
 if (process.argv[1] === new URL(import.meta.url).pathname) {
     console.log(`Speaker run directly, lets test it`);
